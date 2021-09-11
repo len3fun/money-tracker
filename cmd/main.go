a1a2cbfd4f0268f1d1844fb1359b205587d8b534
len3fun <len3fun@gmail.com>
2021-09-11 16:08:43 +0300

    Configuration added

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,27 @@ import (
 	"github.com/len3fun/money-tracker/pkg/handler"
 	"github.com/len3fun/money-tracker/pkg/repository"
 	"github.com/len3fun/money-tracker/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(moneytracker.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
 
-
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}

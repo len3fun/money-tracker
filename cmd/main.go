901d61eeb58ed3794f2573884ee0cc5974e66dd4
len3fun <len3fun@gmail.com>
2021-09-11 13:31:58 +0300

    Base router added

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	moneytracker "github.com/len3fun/money-tracker"
+	"github.com/len3fun/money-tracker/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(moneytracker.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
---
+++ pkg/handler/handler.go
@@ -0,0 +1,26 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api")
+	{
+		sources := api.Group("/sources")
+		{
+			sources.GET("/", h.GetAllSources)
+		}
+	}
+
+	return router
+}

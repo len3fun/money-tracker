3ca8f6518fcb14a7484250759242a9c67578ab6c
len3fun <len3fun@gmail.com>
2021-09-12 09:50:46 +0300

    Auth middleware implemented

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -8,6 +8,7 @@ import (
 type Authorization interface {
 	CreateUser(user moneytracker.User) (int, error)
 	GenerateToken(username, password string) (string, error)
+	ParseToken(token string) (int, error)
 }
 
 type MoneySource interface {
